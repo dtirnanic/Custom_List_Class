67acd844fc4aab7e58d458366b5362f8aea1edb9
unknown <dejan.tirnanic85@gmail.com>
2020-01-24 11:05:11 -0600

    created test methods in UnitTest class to execute check zero index, check add index, check indexs one, two, five and zero index at copied array.

--- CustomList/CustomList.cs
+++ CustomList/CustomList.cs
@@ -9,12 +9,31 @@ namespace CustomList
     public class CustomList<T>
     {
         T[] items;
+        public int capacity = 4;
         public int count;
-        List<int> list = new List<int>();
+        //count represents the number of items in the list (the array)
 
-        public void Add(T items)
+
+        public CustomList()
         {
+            items = new T[capacity]; //assigning (list[array]) capacity
+        }
 
+        public void Add(T item)
+        {
+            //what happens if count and capacity are equal
+            //copy count
+            //add copy to existing count
+            //clear values from new array
+            //add next item in list
+            items[count] = item;
+            count++;
+
+            if (count == capacity)
+            {
+                
+                //items.Clone(capacity);
+            }
         }
     }
 }
--- CustomListTest/UnitTest1.cs
+++ CustomListTest/UnitTest1.cs
@@ -8,7 +8,7 @@ namespace CustomListTest
     public class UnitTest1
     {
 
-        
+
         [TestMethod]
         public void Check_Zero_index_Add()
         {
@@ -20,7 +20,98 @@ namespace CustomListTest
             myList.Add(value);
 
             //Assert
-            Assert.AreEqual(value, myList);
+            Assert.AreEqual(value, myList[0]);
+        }
+
+        [TestMethod]
+        public void Check__index_Add()
+        {
+            //Arrange   
+            CustomList<int> myList = new CustomList<int>();
+            int value = 5;
+            int expectedResult = 1;
+
+            //Arrange
+            myList.Add(value);
+
+            //Assert
+            Assert.AreEqual(expectedResult, myList.count);
+        }
+
+        [TestMethod]
+        public void Check_One_index_Add()
+        {
+            //Arrange   
+            CustomList<int> myList = new CustomList<int>();
+            int value = 5;
+            int value2 = 10;
+
+            //Arrange
+            myList.Add(value);
+            myList.Add(value2);
+
+            //Assert
+            Assert.AreEqual(value, myList[0]);
+        }
+
+        [TestMethod]
+        public void Check_Two_index_Add()
+        {
+            //Arrange   
+            CustomList<int> myList = new CustomList<int>();
+            int value = 5;
+            int value2 = 10;
+
+            //Arrange
+            myList.Add(value);
+            myList.Add(value2);
+
+            //Assert
+            Assert.AreEqual(value2, myList[1]);
+        }
+
+        [TestMethod]
+        public void Check_Five_index_Add()
+        {
+            //Arrange   
+            CustomList<int> myList = new CustomList<int>();
+            int value = 5;
+            int value2 = 10;
+            int value3 = 11;
+            int value4 = 12;
+            int value5 = 13;
+
+            //Arrange
+            myList.Add(value);
+            myList.Add(value2);
+            myList.Add(value3);
+            myList.Add(value4);
+            myList.Add(value5);
+
+            //Assert
+            Assert.AreEqual(value5, myList[4]);
+        }
+
+        [TestMethod]
+        public void Check_Zero_index_Copy_Array()
+        {
+            //Arrange   
+            CustomList<int> myList = new CustomList<int>();
+            int value = 5;
+            int value2 = 10;
+            int value3 = 11;
+            int value4 = 12;
+            int value5 = 13;
+
+            //Arrange
+            myList.Add(value);
+            myList.Add(value2);
+            myList.Add(value3);
+            myList.Add(value4);
+            myList.Add(value5);
+
+            //Assert
+            Assert.AreEqual(value, myList[0]);
         }
     }
 }
